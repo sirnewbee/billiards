d521b874252ccd92de6cf9aacd795789322b1385
cheong.kawin@gmail.com <cheong.kawin@gmail.com>
2019-06-10 17:50:41 +0800

    uploading images

--- src/app/select-game/select-game.component.ts
+++ src/app/select-game/select-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-select-game',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-game.component.sass']
 })
 export class SelectGameComponent implements OnInit {
+  public selectedFile: any = null;
+  public playerPic = document.getElementById('.player-picture');
 
-  constructor() { }
+  constructor(private http: HttpClientModule) {
+   }
 
   ngOnInit() {
   }
-
 }
